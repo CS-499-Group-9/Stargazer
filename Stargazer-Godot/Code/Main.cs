51ca2aab452135e152159915ee30f9e4b72a2dcb
ChargerCoding <165854132+ChargerCoding@users.noreply.github.com>
2025-02-12 17:39:11 -0600

    Screenshot Cam Update
    Made it so the camera switches to the screenshot cam for one frame to take a screenshot. Currently takes a screenshot of the top of the dome, will need to edit this camera. May change it to a separate

--- Stargazer-Godot/Code/Main.cs
+++ Stargazer-Godot/Code/Main.cs
@@ -1,8 +1,10 @@
+using System.Threading.Tasks;
 using Godot;
 
 public partial class Main : Camera3D
 {
     [Export] public float MouseSensitivity = 0.002f;
+    [Export] public Camera3D ScreenshotCam;
     private float yaw = 0f;  // Left/Right Rotation
     private float pitch = 0f; // Up/Down Rotation
     private bool rightClickHeld = false;
@@ -31,8 +33,7 @@ public partial class Main : Camera3D
             Rotation = new Vector3(pitch, yaw, 0);
         }
     }
-    
-    public override void _Process(double delta)
+	public override void _Process(double delta)
     {
         // Check if the 'screenshot_key' action is pressed
         if (Input.IsActionJustPressed("screenshot_key"))
@@ -41,14 +42,26 @@ public partial class Main : Camera3D
         }
     }
 
-    private void TakeScreenshot()
+    private async Task TakeScreenshot()
     {
+        if (ScreenshotCam == null)
+        {
+            GD.Print("No alternative camera assigned!");
+            return;
+        }
+
         // Get the current viewport as an Image
+        Camera3D originalCam = GetViewport().GetCamera3D();
+
+        ScreenshotCam.Current = true;
+        await ToSignal(GetTree(), "process_frame");
         Viewport viewport = GetViewport();
+
         Image screenshotImage = viewport.GetTexture().GetImage();
 
         // Save the screenshot as a JPEG
         screenshotImage.SaveJpg(screenshotPath);
+        originalCam.Current = true;
         GD.Print($"Screenshot saved to {screenshotPath}");
     }
 }
