40f4ccda12abb730633445d7a961e93d87abb4c4
Logan <loganparker0803@gmail.com>
2025-02-17 18:45:38 -0600

    Added basic constellation label and made it toggleable

--- Stargazer-Godot/Code/Spawner.cs
+++ Stargazer-Godot/Code/Spawner.cs
@@ -5,31 +5,32 @@ using System.Collections.Generic;
 public partial class Spawner : Node3D
 {
 	[Export] public PackedScene StarScene {get; set;}
+	[Export] public PackedScene LabelScene {get; set;}
 	
 	private readonly float[,] starPos = {{0, 45, 1}, {10, 45, 2}, {20, 45, 3}, {30, 45, 4}, {40, 45, 5}, {50, 45, 6}};
-	private readonly string[,] cnst_lines = { { "s1", "s2" }, { "s2", "s3" }, { "s3", "s4" }, { "s4", "s5" } };
+	private readonly string[,] constLines = { { "s1", "s2" }, { "s2", "s3" }, { "s3", "s4" }, { "s4", "s5" } };
 	
 	private Globals globalVars;
-	private List<Star> stars = new List<Star>();
+	private List<Star> stars;
 	private Boolean constDrawn = true;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		stars = new List<Star>();
 		globalVars = GetNode<Globals>("/root/Globals");
+		Random rnd = new Random();
 		for (int i = 0; i < starPos.GetLength(0); i++)
 		{
 			stars.Add(SpawnStar(starPos[i, 0], starPos[i, 1], starPos[i,2], $"s{i+1}"));
 		}
-		/* uncomment for random stars
-		 for (int i = 0; i < 2500; i++){
-			stars.Add(SpawnStar((float)rnd.NextDouble() * 360, (float)rnd.NextDouble() * 90, rnd.Next(1, 7)));
+		for (int i = 0; i < 2500; i++){
+			stars.Add(SpawnStar((float)rnd.NextDouble() * 360, (float)rnd.NextDouble() * 90, rnd.Next(1, 7), "s"));
 		}
 		for (int i = 0; i < 2500; i++){
-			stars.Add(SpawnStar((float)rnd.NextDouble() * 360, (float)rnd.NextDouble() * -90, rnd.Next(1, 7)));
-		}*/
-		DrawConstellation(stars, cnst_lines);
+			stars.Add(SpawnStar((float)rnd.NextDouble() * 360, (float)rnd.NextDouble() * -90, rnd.Next(1, 7), "s"));
+		}
+		DrawConstellation(constLines);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -38,24 +39,27 @@ public partial class Spawner : Node3D
 		// Triggers only once to save on performace.
 		if (globalVars.isConstellation && !constDrawn)
 		{
-			DrawConstellation(stars, cnst_lines);
+			DrawConstellation(constLines);
 			constDrawn = true;
 		}
 		else if (!globalVars.isConstellation && constDrawn)
 		{
 			foreach (var child in GetChildren())
 			{
-				if (child is MeshInstance3D)
+				if (child is MeshInstance3D || (child is Node3D label && label.Name == ("Label")))
 					child.QueueFree(); // Remove the constellation line meshes
 			}
 			constDrawn = false;
 		}
 	}
 	
-	private void DrawConstellation(List<Star> stars, string[,] lines)
+	private void DrawConstellation(string[,] lines)
 	{
 		MeshInstance3D constMesh = new MeshInstance3D();
 		ImmediateMesh mesh = new ImmediateMesh();
+		Vector3 labelPos = new Vector3();
+		Vector3 totalPos = new Vector3(0, 0, 0);
+		int c = 0;
 		mesh.SurfaceBegin(Mesh.PrimitiveType.Lines);
 		
 		// Create a white material
@@ -71,10 +75,23 @@ public partial class Spawner : Node3D
 			Star s2 = stars.Find(x => x.starName == lines[i, 1]);
 			mesh.SurfaceAddVertex(s1.Position);
 			mesh.SurfaceAddVertex(s2.Position);
+			if (totalPos == Vector3.Zero) // solely checked for the first star
+			{
+				totalPos += s1.Position;
+				c++;
+			}
+			totalPos += s2.Position;
+			c++;
 		}
 		mesh.SurfaceEnd();
 		constMesh.Mesh = mesh;
 		AddChild(constMesh);
+		
+		// Creating labels
+		labelPos = totalPos / c;
+		Node3D labelNode = LabelScene.Instantiate<Node3D>();
+		labelNode.Position = labelPos;
+		AddChild(labelNode);
 	}
 	
 	private Star SpawnStar(float azimuth, float altitude, float magnitude, string name){
